cd2e3413331d56eb056b03e40e382a417f69a7cd
Jagernout14 <Jagernout@icloud.com>
2025-12-11 11:13:43 +0300

    Доработал замыкания

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -97,7 +97,8 @@ final class MovieQuizViewController: UIViewController {
         imageView.layer.borderColor = isCorrect ? UIColor.ypGreen.cgColor : UIColor.ypRed.cgColor
         imageView.layer.cornerRadius = 20
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0) { [weak self] in
+            guard let self = self else { return }
             self.imageView.layer.borderWidth = 0
                 self.showNextQuestionOrResult()
             }
@@ -126,7 +127,8 @@ final class MovieQuizViewController: UIViewController {
                 message: result.text,
                 preferredStyle: .alert)
             
-            let action = UIAlertAction(title: result.buttonText, style: .default) { _ in
+            let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in
+                guard let self = self else { return }
                 self.currentQuestionIndex = 0
                 self.correctAnswers = 0
                 
