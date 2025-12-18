feb8e4cd5328a70c0f94ba7f6e1a7ae785d96852
Jagernout14 <Jagernout@icloud.com>
2025-12-18 21:52:56 +0300

    Доработал AlertPresenter

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -1,38 +1,20 @@
 import UIKit
 final class AlertPresenter {
     
-    func show(in vc: UIViewController, model: AlertModel) {
-        //Тут я задаю параграф, который выравнивает по центру
-        let paragraphStyle = NSMutableParagraphStyle()
-        paragraphStyle.alignment = .center
-        paragraphStyle.lineBreakMode = .byWordWrapping // Зачем?
-        
-        //Формирую attributedString. Зачем? ЧТо это?
-        let attributedMessage = NSAttributedString(
-            string: model.message,
-            attributes: [
-                .paragraphStyle: paragraphStyle,
-                .font: UIFont.systemFont(ofSize: 13),
-                .foregroundColor: UIColor.label,
-                .kern: 0 //Зачем?
-            ]
-        )
+    func show(in viewController: UIViewController, model: AlertModel) {
         let alert = UIAlertController(
             title: model.title,
-            message: nil,
-            preferredStyle: .alert)
-        alert.setValue(attributedMessage, forKey: "attributedMessage")
+            message: model.message,
+            preferredStyle: .alert
+        )
         
         let action = UIAlertAction(
             title: model.buttonText,
             style: .default
         ) { _ in
-                model.completion()
-            }
+            model.completion()
+        }
         alert.addAction(action)
-        
-        vc.present(alert, animated: true, completion: nil)
+        viewController.present(alert, animated: true)
     }
 }
-//Написал этот код при помощи нейросетки. Вообще, я не понял что тут происходит. То, что непонятно - отметил комментариями
-// И все равно у меня текст в алерте привязан к левому краю -_-
