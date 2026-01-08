a1760d8d1efcf9c71283a36c6ab97fbd1af26564
Jagernout14 <Jagernout@icloud.com>
2026-01-08 18:54:45 +0300

    Забыл убрать нерабочую ссылку на API, исправлено

--- MovieQuiz/Services/MoviesLoader.swift
+++ MovieQuiz/Services/MoviesLoader.swift
@@ -8,7 +8,7 @@ struct MoviesLoader: MoviesLoading {
     private let networkClient = NetworkClient()
     
     private var mostPopularMoviesUrl: URL {
-        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/fake/k_zcuw1ytf") else {
+        guard let url = URL(string: "https://tv-api.com/en/API/Top250Movies/k_zcuw1ytf") else {
             preconditionFailure("Unable to construct mostPopularMoviesUrl")
         }
         return url
